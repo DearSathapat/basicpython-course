fa9ce0099207fe3ced9fcb49dfabd216212c59b4
DearSathapat <101991149+DearSathapat@users.noreply.github.com>
2022-05-03 23:12:19 +0700

    ส่ง Text, image, sticker ไปยังไลน์

---
+++ test-sendline.py
@@ -0,0 +1,8 @@
+from email import message
+from songline import Sendline
+
+token = 'SLJ18X4quHOcGoqAQQPiwMrrRBYcdp0Wo0Y3B9Jci'
+messenger = Sendline(token)
+# messenger.sendtext('Hello world Dear: สวัสดีครับเดียร์')
+# messenger.sticker(11,1)
+messenger.sendimage('https://play-lh.googleusercontent.com/YOwgfSphKOdLCsqRtWTqkxirpgpLIiJtJFuOswzcBUHmcU6fzvvs94WSyI346VyrpQ')
